70bc2dd4d74afe1f540c2c7434c091be293d56a8
Harry Mumford-Turner <harrymt@me.com>
2018-05-07 20:01:47 +0100

    Split up components

--- main.js
+++ main.js
@@ -1,36 +1,47 @@
-const birthday = "November 16";
-const toDays = (1000 * 60 * 60 * 24);
 
-const getNextBirthday = birthday => {
-    const today = new Date();
-    
-    const thisYear = today.getFullYear();
-    const nextYear = thisYear + 1;
-    let nextBirthday = new Date(`${birthday}, ${thisYear}`);
+const Birthday = () => {
+    const birthday = "November 16";
+    const toDays = (1000 * 60 * 60 * 24);
 
-    // If my birthday has past this year
-    if (nextBirthday.valueOf() < today.valueOf()) {
-        nextBirthday = new Date(`${birthday}, ${nextYear}`);
+    const getNextBirthday = birthday => {
+        const today = new Date();
+        
+        const thisYear = today.getFullYear();
+        const nextYear = thisYear + 1;
+        let nextBirthday = new Date(`${birthday}, ${thisYear}`);
+
+        // If my birthday has past this year
+        if (nextBirthday.valueOf() < today.valueOf()) {
+            nextBirthday = new Date(`${birthday}, ${nextYear}`);
+        }
+        return nextBirthday;
     }
-    return nextBirthday;
-}
 
-const millisecondsLeft = (startTime, endTime) => startTime.valueOf() - endTime.valueOf();
-const daysLeft = (birthday, now) => millisecondsLeft(birthday, now) / toDays;
+    const millisecondsLeft = (startTime, endTime) => startTime.valueOf() - endTime.valueOf();
+    const daysLeft = (birthday, now) => millisecondsLeft(birthday, now) / toDays;
 
-// Presentational
-const Amount = ({amount, type}) => {
+    // Presentational
+    const Amount = ({amount, type}) => <span>{amount} {type}</span>;
+
+    const Days = () => {
+        const days = daysLeft(getNextBirthday(birthday), new Date());
+        return <Amount amount={Math.floor(days)} type="days" />
+    }
+
     return (
-        <span>{amount} {type}</span>
+        <h1>Harry's birthday is in <Days/>.</h1>
     );
-};
-
-const Days = () => {
-    const days = daysLeft(getNextBirthday(birthday), new Date());
-    return <Amount amount={Math.floor(days)} type="days" />
 }
+
+const Footer = ({link}) => 
+    <footer>
+        <a href={link}>GitHub</a>
+    </footer>
 
 ReactDOM.render(
-    <h1>My Birthday is in <Days/>.</h1>,
+    <div className="container">
+        <Birthday />
+        <Footer link="//github.com/harrymt/birthday" />
+    </div>,
     document.getElementById('root')
 );
